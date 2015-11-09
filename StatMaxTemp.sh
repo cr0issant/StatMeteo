1b4204296f71cfd5a06e351dff4ac4bef3a42d82
cr0issant <matthieu.pradeau@aquilenet.fr>
2015-11-09 10:26:05 +0100

    Ajout modif variable

--- StatMaxTemp.sh
+++ StatMaxTemp.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-FileCsv="StatMax-08-11.csv"
-Lieu="http://www.meteofrance.com/climat/meteo-date-passee?lieuId=243400&lieuType=VILLE_FRANCE&date=08-11-"
+echo "Recherche des températures maximales d\'un jour de l\'année sur plusieurs années"
+echo 'Code commune ?'
+read CodeCommune
+CodeCommune+='0'
+echo 'Jour ? ( Format : 05, 14 )'
+read Jour
+echo 'Mois ?'
+read Mois
+echo 'Année de départ ?'
+read Start
+echo 'Année de fin ?'
+read End
+
+FileCsv="StatMax-$Jour-$Mois.csv"
+Lieu="http://www.meteofrance.com/climat/meteo-date-passee?lieuId=$CodeCommune&lieuType=VILLE_FRANCE&date=$Jour-$Mois-"
 
 echo "Température maximale de la journée" >> $FileCsv
 echo "Année;Max" >> $FileCsv
 
-for i in $(seq 2010 2014);
+for i in $(seq $Start $End);
 do
-	#echo "08-11-$i" >> $FileCsv 
+	#echo "$Jour-$Mois-$i" >> $FileCsv 
 	curl "$Lieu$i" | grep maximale\ de\ la\ journée >> $FileCsv
-	sed -i -e "s/                                                                    <li>Température maximale de la journée : /08-11-$i;/g" $FileCsv
+	sed -i -e "s/                                                                    <li>Température maximale de la journée : /$Jour-$Mois-$i;/g" $FileCsv
 	sed -i -e "s/°C<\/li>//g" $FileCsv
 	sed -i -e "s///" $FileCsv
 	sed -i -e "s/ : /;/g" $FileCsv
